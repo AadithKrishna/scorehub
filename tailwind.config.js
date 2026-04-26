129ca4afa7f6a72cf3d0cb8b657cd3aa1b948806
AadithKrishna <krishna.aadith@gmail.com>
2026-04-26 23:02:46 +0200

    UI Style fix new iteration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        surface: 'var(--surface)',
+        border:  'var(--border)',
+      }
+    }
+  },
   plugins: [],
 }
